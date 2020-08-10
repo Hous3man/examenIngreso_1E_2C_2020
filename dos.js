8fcf2277af9f7b9801ee1490ca90c8c7d7e84499
Damián D'Arrigo <ddarrigo.87@gmail.com>
2020-08-10 20:45:54 -0300

    ej2y3

--- dos.js
+++ dos.js
@@ -5,93 +5,58 @@ marca, precio, peso en kilogramos, tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  function mostrar() {
+*/function mostrar() {
   let marca = "";
   let precio = 0;
-    
+  let peso = 0;
+  let tipo = "";
+  let resp;
+  let acumPeso = 0;
+  let precioSolido = 0;
+  let precioMasCaroSolido = 0;
+  let precioLiquido = 0;
+  let precioMasBaratoLiquido = 0;
+  let marcaMasCaraSolido = "No se ingresaron sólidos";
+  let marcaMasBarataLiquido = "No se ingresaron líquidos";
+  let flagLiquido = 0;
+
+  do {
+    marca = prompt("Ingrese la marca: ");
     do {
-      marca = prompt("Ingrese la marca: ");
-      do {
-        precio = parseInt(prompt("Ingrese el precio: "));
-      }while (isNaN(precio) || precio <= 0);
-      do {
-        peso = parseInt(prompt("Ingrese el peso: "));
-      } while (isNaN(peso) || peso <=0);
-      
-      switch (vTipo) {
-        case "Arena":
-          acumArena = acumArena + vCantBolsas;
-          vPrecioArena = vPrecioArena + (vPrecioPorBolsa*vCantBolsas);
-          if(vPrecioMaxArena < vPrecioPorBolsa)
-          {
-            vPrecioMaxArena = vPrecioPorBolsa;
-          }
-          break;
-        case "Cal":
-          acumCal = acumCal + vCantBolsas;
-          vPrecioCal = vPrecioCal + (vPrecioPorBolsa*vCantBolsas);
-          if(vPrecioMaxCal < vPrecioPorBolsa)
-          {
-            vPrecioMaxCal = vPrecioPorBolsa;
-          }
-          break;
-        case "Cemento":
-          acumCemento = acumCemento + vCantBolsas;
-          vPrecioCemento = vPrecioCemento + (vPrecioPorBolsa*vCantBolsas);
-          if(vPrecioMaxCemento < vPrecioPorBolsa)
-          {
-            vPrecioMaxCemento = vPrecioPorBolsa;
-          }
-          break;
+      precio = parseInt(prompt("Ingrese el precio: "));
+    } while (isNaN(precio) || precio <= 0);
+    do {
+      peso = parseInt(prompt("Ingrese el peso: "));
+    } while (isNaN(peso) || peso <= 0);
+    do {
+      tipo = prompt("Ingrese el tipo de producto: ");
+    } while (tipo != "Sólido" && tipo != "Líquido");
+    acumPeso = acumPeso + peso;
+    switch(tipo){
+      case "Sólido":
+      precioSolido = precio;
+      if(precioMasCaroSolido < precioSolido){
+        precioMasCaroSolido = precioSolido;
+        marcaMasCaraSolido = marca;
       }
-      resp = prompt("Desea ingresas otro producto? si/no ");
-      while(resp != "si" && resp != "no"){
+      break;
+      case "Líquido":
+      precioLiquido = precio;
+      if(flagLiquido == 0 || precioMasBaratoLiquido > precioLiquido){
+        precioMasBaratoLiquido = precioLiquido;
+        marcaMasBarataLiquido = marca;
+        flagLiquido = 1;
+      }
+      break;
+    }
+    resp = prompt("Desea ingresas otro producto? si/no ");
+    while (resp != "si" && resp != "no") {
       resp = prompt("Respuesta inválida. Ingrese si o no");
-      }
-    } while (resp == "si");
-    
-    vCantBolsasTotal = acumArena + acumCal + acumCemento;
-    vPrecioTotal = vPrecioArena + vPrecioCal + vPrecioCemento;
-  
-    if (vCantBolsasTotal >= 10) {
-      vPrecioConDesc = vPrecioTotal * 0.85;
     }
-    else if (vCantBolsasTotal >= 30) {
-      vPrecioConDesc = vPrecioTotal * 0.75;
-    } else {
-      vPrecioSinDesc = vPrecioTotal;
-    }
-  
-    if (acumArena > acumCal && acumArena > acumCemento) {
-      vMayorCant = "Arena";
-    }
-    else if (acumCal >= acumArena && acumArena > acumCemento) {
-      vMayorCant = "Cal";
-    }
-    else {
-      vMayorCant = "Cemento";
-    }
-  
-    if(vCantBolsasTotal < 10){
-    alert("Usted abonó, bruto sin descuento: $"+vPrecioSinDesc);
-    }else {
-    alert("Usted abonó, bruto sin descuento: $"+ vPrecioTotal);
-    alert("Usted abonó, con descuento: $"+ vPrecioConDesc);
-    }
-    
-    alert("El tipo con más cantidad es: "+ vMayorCant);
-  
-    if (vPrecioMaxArena > vPrecioMaxCal && vPrecioMaxArena > vPrecioMaxCemento) {
-      vTipoMasCaro = "Arena";
-    }
-    else if (vPrecioMaxCal >= vPrecioMaxArena && vPrecioMaxCal > vPrecioMaxCemento) {
-      vTipoMasCaro = "Cal";
-    }
-    else {
-      vTipoMasCaro = "Cemento";
-    }
-    alert("El tipo más caro es: "+ vTipoMasCaro);
-  }
+  } while (resp == "si");
+
+  document.write("Peso de la compra: " + acumPeso+"<br>");
+	document.write("Marca más cara sólidos: " + marcaMasCaraSolido +"<br>");
+	document.write("Marca más barata líquidos: " + marcaMasBarataLiquido +"<br>");
+
 }
--- tres.js
+++ tres.js
@@ -9,7 +9,90 @@ a)el lugar más elegido
 b)el sexo de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+	let sexo = "";
+	let lugar = "";
+	let temporada = "";
+	let cantidad = 0;
+	let resp = "";
+	let contBariloche = 0;
+	let contCataratas = 0;
+	let contSalta = 0;
+	let contFem = 0;
+	let contMasc = 0;
+	let lugMasElegido = "";
+	let sexMasPasajeros = 0;
+	let acumInvierno = 0;
+	let contInvierno = 0;
+	let promViajeInv = 0;
+
+	do {
+		do {
+			sexo = prompt("Ingrese su sexo, f o m: ");
+			switch (sexo) {
+				case "Femenino":
+				case "Mujer":
+				case "F":
+					sexo = "f";
+					contFem++;
+					break;
+				case "Masculino":
+				case "Hombre":
+				case "M":
+					sexo = "m";
+					contMasc++
+					break;
+			}
+		} while (sexo != "f" && sexo != "m");
+		do {
+			lugar = prompt("Ingrese el lugar: ");
+		} while (lugar != "Bariloche" && lugar != "Cataratas" && lugar != "Salta");
+		do {
+			temporada = prompt("Ingrese el temporada: ");
+		} while (temporada != "Otoño" && temporada != "Invierno" && temporada != "Verano" && temporada != "Primavera");
+		do {
+			cantidad = parseInt(prompt("Ingrese la cantidad de viajantes: "));
+		} while (isNaN(cantidad) || cantidad <= 0);
+
+		switch (lugar) {
+			case "Bariloche":
+				contBariloche++;
+				break;
+			case "Cataratas":
+				contCataratas++;
+				break;
+			case "Salta":
+				contSalta++;
+				break;
+		}
+
+		if(temporada == "Invierno"){
+			acumInvierno = acumInvierno + cantidad;
+			contInvierno++
+			promViajeInv = acumInvierno/contInvierno;
+		}
+
+		resp = prompt("Desea ingresar otra estadía? si/no ");
+		while (resp != "si" && resp != "no") {
+			resp = prompt("Respuesta inválida. Ingrese si o no");
+		}
+	} while (resp == "si")
+
+	if (contBariloche > contCataratas && contBariloche > contSalta) {
+		lugMasElegido = "Bariloche";
+	} else if (contCataratas >= contBariloche && contCataratas > contSalta) {
+		lugMasElegido = "Cataratas";
+	} else {
+		lugMasElegido = "Salta";
+	}
+
+	if (contFem > contMasc) {
+		sexMasPasajeros = "Mujeres";
+	} else {
+		sexMasPasajeros = "Hombres";
+	}
+
+	document.write("Lugar más elegido: " + lugMasElegido+"<br>");
+	document.write("Sexo de titular que lleva más pasajeros. " + sexMasPasajeros +"<br>");
+	document.write("Promedio de personas por viaje, que viajan en invierno: " + promViajeInv +"<br>");
 }
