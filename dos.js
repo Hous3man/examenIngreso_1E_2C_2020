8e10ca9d62e9011192e8c7e7a0ffbee950666203
Damián D'Arrigo <ddarrigo.87@gmail.com>
2020-08-10 19:43:09 -0300

    ej1

--- dos.js
+++ dos.js
@@ -7,5 +7,91 @@ b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  function mostrar() {
+  let marca = "";
+  let precio = 0;
+    
+    do {
+      marca = prompt("Ingrese la marca: ");
+      do {
+        precio = parseInt(prompt("Ingrese el precio: "));
+      }while (isNaN(precio) || precio <= 0);
+      do {
+        peso = parseInt(prompt("Ingrese el peso: "));
+      } while (isNaN(peso) || peso <=0);
+      
+      switch (vTipo) {
+        case "Arena":
+          acumArena = acumArena + vCantBolsas;
+          vPrecioArena = vPrecioArena + (vPrecioPorBolsa*vCantBolsas);
+          if(vPrecioMaxArena < vPrecioPorBolsa)
+          {
+            vPrecioMaxArena = vPrecioPorBolsa;
+          }
+          break;
+        case "Cal":
+          acumCal = acumCal + vCantBolsas;
+          vPrecioCal = vPrecioCal + (vPrecioPorBolsa*vCantBolsas);
+          if(vPrecioMaxCal < vPrecioPorBolsa)
+          {
+            vPrecioMaxCal = vPrecioPorBolsa;
+          }
+          break;
+        case "Cemento":
+          acumCemento = acumCemento + vCantBolsas;
+          vPrecioCemento = vPrecioCemento + (vPrecioPorBolsa*vCantBolsas);
+          if(vPrecioMaxCemento < vPrecioPorBolsa)
+          {
+            vPrecioMaxCemento = vPrecioPorBolsa;
+          }
+          break;
+      }
+      resp = prompt("Desea ingresas otro producto? si/no ");
+      while(resp != "si" && resp != "no"){
+      resp = prompt("Respuesta inválida. Ingrese si o no");
+      }
+    } while (resp == "si");
+    
+    vCantBolsasTotal = acumArena + acumCal + acumCemento;
+    vPrecioTotal = vPrecioArena + vPrecioCal + vPrecioCemento;
+  
+    if (vCantBolsasTotal >= 10) {
+      vPrecioConDesc = vPrecioTotal * 0.85;
+    }
+    else if (vCantBolsasTotal >= 30) {
+      vPrecioConDesc = vPrecioTotal * 0.75;
+    } else {
+      vPrecioSinDesc = vPrecioTotal;
+    }
+  
+    if (acumArena > acumCal && acumArena > acumCemento) {
+      vMayorCant = "Arena";
+    }
+    else if (acumCal >= acumArena && acumArena > acumCemento) {
+      vMayorCant = "Cal";
+    }
+    else {
+      vMayorCant = "Cemento";
+    }
+  
+    if(vCantBolsasTotal < 10){
+    alert("Usted abonó, bruto sin descuento: $"+vPrecioSinDesc);
+    }else {
+    alert("Usted abonó, bruto sin descuento: $"+ vPrecioTotal);
+    alert("Usted abonó, con descuento: $"+ vPrecioConDesc);
+    }
+    
+    alert("El tipo con más cantidad es: "+ vMayorCant);
+  
+    if (vPrecioMaxArena > vPrecioMaxCal && vPrecioMaxArena > vPrecioMaxCemento) {
+      vTipoMasCaro = "Arena";
+    }
+    else if (vPrecioMaxCal >= vPrecioMaxArena && vPrecioMaxCal > vPrecioMaxCemento) {
+      vTipoMasCaro = "Cal";
+    }
+    else {
+      vTipoMasCaro = "Cemento";
+    }
+    alert("El tipo más caro es: "+ vTipoMasCaro);
+  }
 }
--- uno.js
+++ uno.js
@@ -7,7 +7,61 @@ b)la edad promedio en total.
 c)el hombre mas pesado.
 pedir datos por prompt y mostrar por document.write o console.log
 */
-function mostrar()
-{
-	alert("uno");
+function mostrar() {
+	let nombre = "";
+	let peso = 0;
+	let sexo = "";
+	let edad = "";
+	let contMujer = 0;
+	let acumEdad = 0;
+	let contEdad = 0;
+	let promEdad = 0;
+	let pesoHomMasPesado = 0;
+	let	nombreHomMasPesado = "";
+
+
+	for (i = 1; i <= 5; i++) {
+		nombre = prompt("Ingrese un nombre: ")
+		do {
+			peso = parseInt(prompt("Ingrese su peso: "));
+		} while (isNaN(peso) || peso <= 20 || peso > 300);
+		do {
+			sexo = prompt("Ingrese su sexo, f o m: ");
+			switch (sexo) {
+				case "Femenino":
+				case "Mujer":
+				case "F":
+					sexo = "f";
+					break;
+				case "Masculino":
+				case "Hombre":
+				case "M":
+					sexo = "m";
+					break;
+			}
+		} while (sexo != "f" && sexo != "m");
+		do {
+			edad = parseInt(prompt("Ingrese su edad: "));
+		} while (isNaN(edad) || edad <= 0 || edad > 110);
+
+		acumEdad = acumEdad + edad;
+		contEdad++;
+		if (sexo == "f") {
+			contMujer++;
+		}
+		if (sexo == "m" && pesoHomMasPesado < peso){
+			pesoHomMasPesado = peso;
+			nombreHomMasPesado = nombre;
+		}
+	}
+
+	if (contEdad == 0) {
+		promEdad = promEdad / 1;
+	} else {
+		promEdad = acumEdad / contEdad;
+	}
+
+	document.write("Cantidad de mujeres: " + contMujer+"<br>");
+	document.write("Edad promedio: " + promEdad.toFixed(2) + " años"+"<br>");
+	document.write("El hombre más pesado: " + nombreHomMasPesado +"<br>");
 }
